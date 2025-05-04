d07783ab9e3df2edb3daa6ab96a739c05c118ef9
thedraxx <thedraxxgames@gmail.com>
2025-05-04 14:08:39 -0300

    Zoneless Angular

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,22 +1,40 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
-  template: `
-    <h1>Counter</h1>
-    <h2>{{ counter }}</h2>
-    <button (click)="increaseByOne()">Increment</button>
-    <button (click)="decreaseByOne()">Decrement</button>
-  `,
-
+  templateUrl: "./counter-page.component.html",
+  styles:
+    [
+      `
+      h1 {
+        color: #fff;
+        text-align: center;
+      }
+      .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+      button {
+        margin: 5px;
+      }
+    `,
+    ],
 })
 export class CounterPageComponent {
   counter = 10;
-
+  counterSignal = signal(10);
 
   increaseByOne() {
     this.counter = this.counter + 1;
+    this.counterSignal.update((current) => current + 1);
   }
   decreaseByOne() {
     this.counter = this.counter - 1;
+  }
+
+  resetCounter() {
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
